cb26a2405cde609aca7942ac0c21eb43fb1564e8
Livia-Rose <livia.rose@s.amity.adu>
2025-03-25 14:35:40 +0530

    case 4

---
+++ case4.c
@@ -0,0 +1,10 @@
+case 4:
+            printf("Exiting the program. Goodbye!\n");
+            break;
+        default:
+            printf("Invalid choice! Please enter a number between 1 and 4.\n");
+    }
+    
+    return 0;
+}
+
